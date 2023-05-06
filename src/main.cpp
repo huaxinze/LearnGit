3281bf7cb29e9702ea7af4f0ce04d1b7f2e5e201
huaxinze <huaxinze@qq.com>
2023-05-06 22:19:03 +0800

    Merge branch 'master' of github.com:huaxinze/LearnGit into feature

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,6 @@ int a = 10;
 
 int main()
 {
-<<<<<<< HEAD
-    std::cout << "current branch is C" << std::endl;
-=======
     std::cout << "current branch is E" << std::endl;
->>>>>>> fb1533177400826cbb503327af5757b3f71873a3
     return 0;
 }
