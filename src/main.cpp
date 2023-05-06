7dd00e93cbc350699ae56a6b5d4202f0360b74c7
huaxinze <huaxinze@qq.com>
2023-05-06 22:37:52 +0800

    master brach commit thrid time
    Signed-off-by: huaxinze <huaxinze@qq.com>

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,6 @@ int a = 10;
 int main()
 {
     std::cout << "current branch is E" << std::endl;
-    std::cout << "master brach commit second time" << std::endl;
+    std::cout << "master brach commit thrid time" << std::endl;
     return 0;
 }
