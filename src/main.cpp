4c97d3cb1ebf5ecfacf6ef448b999ff6abb51f4e
huaxinze <huaxinze@qq.com>
2023-05-08 23:14:13 +0800

    master brach commit fifth time
    Signed-off-by: huaxinze <huaxinze@qq.com>

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ int a = 10;
 int main()
 {
     std::cout << "current branch is E" << std::endl;
-    std::cout << "master brach commit forth time" << std::endl;
+    std::cout << "master brach commit fifth time" << std::endl;
     std::cout << "feature brach modify and merge" << std::endl;
 
     // step1:feature 分支开发代码
@@ -15,5 +15,7 @@ int main()
     // step3:checkout到master分支，并git pull origin master同步最新的master分支代码
     // step4:git merge feature 请求合并feature分支，会存在冲突，需要处理
     // step5:git commit -a 提交合并
+
+    // how to use git rebase
     return 0;
 }
