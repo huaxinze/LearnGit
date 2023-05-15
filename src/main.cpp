aa3af1439a2cc13d3552ec219a43b0bb558259dd
huaxinze <huaxinze@qq.com>
2023-05-15 22:09:36 +0800

    commit by master
    Signed-off-by: huaxinze <huaxinze@qq.com>

--- src/main.cpp
+++ src/main.cpp
@@ -19,5 +19,11 @@ int main()
     // step5:git commit -a 提交合并
 
     // how to use git rebase
+    // step1:feature 分支开发代码
+    // step2:feature 分支提交代码
+    // step3:切换到master 分支更新到最新代码
+    // step4:切换到feature 并git rebase master解决冲突
+    // step5:提交feature分支代码
+    // step6:在远程提交merge
     return 0;
 }
